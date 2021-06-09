42be7677fee49aa9d212606ee35db08deec61503
Misinciuc <iuriemisinciuc18@gmail.com>
2021-06-09 22:04:01 +0300

    Lesson37

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
-import { state, NewsAdd } from './redux/Store';
-import { RenderTree } from './render';
+import App from './App'
+import store from './redux/Store'
 
-RenderTree(state);
+let RenderTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={store.getState()}
+                NewsAdd={store.NewsAdd.bind(store)}
+                Update_News={store.Update_News.bind(store)}
+                Message_Add={store.Message_Add.bind(store)}
+                Message_Update={store.Message_Update.bind(store)} />
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
+}
+RenderTree(store.getState());
 
-
+store.subscribe(RenderTree);
 
 
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,58 +1,64 @@
-import { RenderTree } from "../render";
-
-export let state = {
-  MesagePage: {
-    Refresh: "enter message",
-    DialConv: [
-      { id: 1, name: "Iurcik" },
-      { id: 2, name: "John" },
-      { id: 3, name: "Peter" },
-      { id: 4, name: "David" },
-    ],
-    DialTxt: [
-      { id: 1, message: "Hello people" },
-      { id: 2, message: "Just do it" },
-      { id: 3, message: "YO YO" },
-      { id: 4, message: "fifa" },
-    ],
+let store = {
+  _state: {
+    MesagePage: {
+      Refresh: "enter message",
+      DialConv: [
+        { id: 1, name: "Iurcik" },
+        { id: 2, name: "John" },
+        { id: 3, name: "Peter" },
+        { id: 4, name: "David" },
+      ],
+      DialTxt: [
+        { id: 1, message: "Hello people" },
+        { id: 2, message: "Just do it" },
+        { id: 3, message: "YO YO" },
+        { id: 4, message: "fifa" },
+      ],
+    },
+    NewsPage: {
+      NewMesTxt: "news",
+      MainNews: [
+        { id: 1, news: "wheather" },
+        { id: 2, news: "It is a beautiful day" },
+        { id: 3, news: "Mix mix mix" },
+        { id: 4, news: "outsideß" },
+      ],
+    },
   },
-  NewsPage: {
-    NewMesTxt: "news",
-    MainNews: [
-      { id: 1, news: "wheather" },
-      { id: 2, news: "It is a beautiful day" },
-      { id: 3, news: "Mix mix mix" },
-      { id: 4, news: "outsideß" },
-    ],
+  getState() {
+    return this._state
   },
-}
-
-export const NewsAdd = () => {
-  let newtxt = {
-    id: 5,
-    news: state.NewsPage.NewMesTxt
-  };
-  state.NewsPage.MainNews.push(newtxt);
-  state.NewsPage.NewMesTxt = '';
-  RenderTree(state);
-}
-
-export const Update_News = (fignea) => {
-  state.NewsPage.NewMesTxt = fignea;
-  RenderTree(state);
-}
-
-export const Message_Add = () => {
-  let insert = {
-    id: 5,
-    message: state.MesagePage.Refresh
+  RenderTree() {
+    console.log('change');
+  },
+  NewsAdd() {
+    let newtxt = {
+      id: 5,
+      news: this._state.NewsPage.NewMesTxt
+    };
+    this._state.NewsPage.MainNews.push(newtxt);
+    this._state.NewsPage.NewMesTxt = '';
+    this.RenderTree(this._state);
+  },
+  Update_News(fignea) {
+    this._state.NewsPage.NewMesTxt = fignea;
+    this.RenderTree(this._state);
+  },
+  Message_Add() {
+    let insert = {
+      id: 5,
+      message: this._state.MesagePage.Refresh
+    }
+    this._state.MesagePage.DialTxt.push(insert);
+    this._state.MesagePage.Refresh = '';
+    this.RenderTree(this._state);
+  },
+  Message_Update(mess) {
+    this._state.MesagePage.Refresh = mess;
+    this.RenderTree(this._state);
+  },
+  subscribe(observer) {
+    this.RenderTree = observer;
   }
-  state.MesagePage.DialTxt.push(insert);
-  state.MesagePage.Refresh = '';
-  RenderTree(state);
 }
-
-export const Message_Update = (mess) => {
-  state.MesagePage.Refresh = mess;
-  RenderTree(state);
-}
+export default store;
